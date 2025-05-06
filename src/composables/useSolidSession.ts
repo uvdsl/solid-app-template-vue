f82d20acb3c0b5b131d729f7883e754ff52b4c16
Christoph Braun <christoph.braun@protonmail.com>
2025-05-06 13:37:28 +0200

    refactored session

--- src/composables/useSolidSession.ts
+++ src/composables/useSolidSession.ts
@@ -1,16 +1,23 @@
+import { ClientDetails, Session } from "@uvdsl/solid-oidc-client-browser";
 import { reactive } from "vue";
-import { Session } from "@uvdsl/solid-oidc-client-browser";
 
 interface IuseSolidSession {
   session: Session;
   restoreSession: () => Promise<void>;
 }
 
-const session = reactive(new Session());
+const clientDetails: ClientDetails = {
+  client_id: "https://uvdsl.solid.aifb.kit.edu/test/client_id.jsonld",
+  redirect_uris: [window.location.href],
+  client_name: "uvdsl's Solid App Template"
+};
+
+const session = reactive(new Session(clientDetails));
 
 async function restoreSession() {
   await session.handleRedirectFromLogin();
 }
+
 /**
  * Auto-re-login / and handle redirect after login
  * 
